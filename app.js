18bc4ab53d2775d1bbef852dbf14ce0005b5a24c
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-07-06 23:59:46 -0500

    2

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+window.scroll = function () {
+    getStickyNavBar ();
+}
+// console.log("Hello");
+const navBar = document.querySelector (".navBar");
+console.log(navBar);
+const stickyNavBarOffset = navBar.offsetTop;
+console.log(stickyNavBarOffset);
+
+const getStickyNavBar = () => {
+    if (window.pageYOffset >= stickyNavBarOffset) {
+        navBar.classList.add ("sticky");
+    } else {
+        navBar.classList.remove ("sticky");
+    }
+}
