a181f3791c7945502b0a1efadd27202bb0588231
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-07-08 22:34:10 -0500

    Fixed sticky menu and div padding and margin

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-window.scroll = function () {
-    getStickyNavBar ();
-}
-// console.log("Hello");
-const navBar = document.querySelector (".navBar");
-console.log(navBar);
-const stickyNavBarOffset = navBar.offsetTop;
-console.log(stickyNavBarOffset);
-
-const getStickyNavBar = () => {
-    if (window.pageYOffset >= stickyNavBarOffset) {
-        navBar.classList.add ("sticky");
-    } else {
-        navBar.classList.remove ("sticky");
-    }
-}
+console.log("Hello");
